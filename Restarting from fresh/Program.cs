8591f6fa962e5362b9d2a86b61f8e861aaf4432a
shalom mande <shalom.mande@gmail.com>
2020-12-30 21:37:00 -0500

    Add project files.

---
+++ Restarting from fresh/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Restarting_from_fresh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+
+            x = 50 - 10 ;
+
+            Console.WriteLine(x);
+        }
+    }
+}
